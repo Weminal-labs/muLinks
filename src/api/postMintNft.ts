ccefdf5a0c46c298e7785308f7036dc2234cf002
Kay (Andrew) <chauanhtuan185@gmail.com>
2024-10-21 13:07:31 +0700

    feat: convenrt evm address to subtrace address

--- src/api/postMintNft.ts
+++ src/api/postMintNft.ts
@@ -1,28 +1,29 @@
 import { Request, Response } from "express";
 import { UniqueChain } from '@unique-nft/sdk';
-import { encodeAddress , decodeAddress } from '@polkadot/util-crypto';
-
+import { encodeAddress , decodeAddress , blake2AsU8a} from '@polkadot/util-crypto';
+import { hexToU8a , stringToU8a , u8aConcat  } from '@polkadot/util'
 export async function postMintNft(req: Request, res: Response): Promise<void> { 
-    const { address } = req.body;
+    
+    const { address } = req.body;   
+    const a8u = hexToU8a(address)
+    const data = stringToU8a("evm:");
+    const res_ = blake2AsU8a(u8aConcat(data, a8u));
+    const output = encodeAddress(res_, 42);
     try {
         const sdk = await UniqueChain({
             baseUrl: 'https://rest.unique.network/v2/opal',  
         })
-        const decodedAddress = decodeAddress(address);
-        const substrateAddress = encodeAddress(decodedAddress);
-        console.log(substrateAddress)
-
         const id: string | number = 4505
         const transaction = await sdk.token.mintNFTs.build({
             collectionId: id,
             tokens: [
                 {
-                    owner: substrateAddress,
+                    owner: output,
                     data: { image: 'https://gateway.pinata.cloud/ipfs/QmTkhTg5S5zrqJL3UsKtyiFi8fcMT3Cao9uKtadp3Ckh7m'}
                 },
               ],
         }, {
-            signerAddress: substrateAddress,
+            signerAddress: output,
             nonce: undefined,  
             mortalLength: undefined
         }
